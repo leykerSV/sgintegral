e3886964e8a6ba4016e7c4f5dea5a711badeb9ea
Leyker <dleyendeker@gmail.com>
2021-02-03 20:12:59 -0300

    Primer Guardado Sistema de Gestión

---
+++ application/views/comunes/footer.php
@@ -0,0 +1,44 @@
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+<!-- jQuery -->
+<script src="assets/tema/plugins/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="assets/tema/plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="assets/tema/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- ChartJS -->
+<script src="assets/tema/plugins/chart.js/Chart.min.js"></script>
+<!-- Sparkline -->
+<script src="assets/tema/plugins/sparklines/sparkline.js"></script>
+<!-- JQVMap -->
+<script src="assets/tema/plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="assets/tema/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="assets/tema/plugins/jquery-knob/jquery.knob.min.js"></script>
+<!-- daterangepicker -->
+<script src="assets/tema/plugins/moment/moment.min.js"></script>
+<script src="assets/tema/plugins/daterangepicker/daterangepicker.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="assets/tema/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Summernote -->
+<script src="assets/tema/plugins/summernote/summernote-bs4.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="assets/tema/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="assets/tema/dist/js/adminlte.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="assets/tema/dist/js/demo.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="assets/tema/dist/js/pages/dashboard.js"></script>
+</body>
+</html>
